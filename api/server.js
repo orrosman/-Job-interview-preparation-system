0cade0bc19e7dab9d66cf6079fb5328cf73a9eab
Or Rosman <42673555+orrosman@users.noreply.github.com>
2021-11-14 01:09:49 +0200

    final heroku deployment

--- api/server.js
+++ api/server.js
@@ -7,12 +7,13 @@ const errorHandler = require('./middleware/errorHandler');
 const app = new express();
 app.use(cors());
 
+app.use(express.static(path.join(__dirname, './dist')));
+app.get('', (req, res) => {
+	res.sendFile(path.join(__dirname, './api/dist/index.html'));
+});
+
 app.use(express.json());
 app.use('/', questionsRouter);
-app.use('/', express.static(path.resolve('./dist')));
-app.get('/', function (req, res) {
-	res.sendFile(__dirname, './dist/index.html');
-});
 app.use(errorHandler);
 
 app.listen(process.env.PORT || 3000, () => {
